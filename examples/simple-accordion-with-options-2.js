41985d3204e9fe63df4a231559c708db6d36e8ea
Kuldeep Keshwar <kuldeepkeshwar@gmail.com>
2015-10-12 12:09:32 +0530

    add examples

---
+++ examples/simple-accordion-with-options-2.js
@@ -0,0 +1,33 @@
+angular.module('myApp',['uiAccordion']);
+angular.module('myApp').controller("myCtrl",function($scope){
+
+    $scope.simpleAccordionOptions={
+        closeOthers:false  //Default: true) - Control whether expanding an item will cause the other items to close.
+    }
+    $scope.accordionGroupOptions1={
+        open:true          //(Default: false) - Whether accordion group is open or closed.
+    }
+    $scope.accordionGroupOptions2={
+        open:false,
+        disabled:true      //(Default: false) - Whether the accordion group is disabled or not.
+    }
+    $scope.accordionGroupOptions3={
+        open:true,
+        beforeOpen:function(){   //promise which is resolved before showing accordion body
+            var def=$q.defer();
+            // busy doing some stuff
+            setTimeout(function(){
+                def.resolve();
+            },3000)
+            return def.promise;
+        },
+        beforeHide:function(){  //promise which is resolved before hidding accordion body
+            var def=$q.defer();
+            // busy doing some stuff
+            setTimeout(function(){
+                def.resolve();
+            },3000)
+            return def.promise;
+        }
+    }
+});
