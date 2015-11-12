a9de2ba6850f46358134dacdc64dc2067aa93f29
kuldeep keshwar <kuldeepkeshwar@gmail.com>
2015-11-12 12:49:25 +0530

    Update ng-accordion-group.js

--- test/spec/directives/ng-accordion-group.js
+++ test/spec/directives/ng-accordion-group.js
@@ -12,7 +12,7 @@ describe('Directive: ngAccordionGroup', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
+  xit('should make hidden element visible', inject(function ($compile) {
     element = angular.element('<ng-accordion-group></ng-accordion-group>');
     element = $compile(element)(scope);
     expect(element.text()).toBe('this is the ngAccordionGroup directive');
