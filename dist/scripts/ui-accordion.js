ebb81cd5419924eb8e9478a8319a984b95c5da6f
Kuldeep Keshwar <kuldeepkeshwar@KuldeepMac.local>
2016-03-23 12:42:28 +0530

    Release v1.2.3

--- dist/scripts/ui-accordion.js
+++ dist/scripts/ui-accordion.js
@@ -38,7 +38,65 @@ angular.module('uiAccordion')
       };
   });
 
-'use strict';/** * @ngdoc directive * @name uiAccordion.directive:ngAccordionGroup * @description * # ngAccordionGroup */angular.module('uiAccordion')  .directive('ngAccordionGroup', function (accordionGroup,$q,$timeout) {      return {          require: ['^ngAccordion', 'ngAccordionGroup'],          restrict: 'EA',          controller: 'NgAccordionGroupCtrl',          scope: {              options: '='          },          link: function (scope, element, attrs, controllers) {              var accordionCtrl, controller, accordion, _accordionGroup;              controller = controllers[1];              accordionCtrl = controllers[0];              accordion = accordionCtrl.accordion;              _accordionGroup = accordionGroup.createAccordionGrp();              if (!scope.options) {                  scope.options = accordionGroup.defaultAccordionGroupOptions();              }              _accordionGroup.options = angular.extend(accordionGroup.defaultAccordionGroupOptions(), scope.options);              scope.$watchCollection('options', function (n) {                  _accordionGroup.options = angular.extend(accordionGroup.defaultAccordionGroupOptions(), n);                  if (n && !n.disabled) {                      accordion.applyState(_accordionGroup);                  }              }, true);              $q.all([controller.getHeaderElement(), controller.getBodyElement()]).then(function (results) {                  _accordionGroup.header = results[0];                  _accordionGroup.body = results[1];                  if (scope.options.open) {                      _accordionGroup.$animate('show', 'beforeOpen', 'animateOpen');                  } else {                      _accordionGroup.$animate('hide', 'beforeHide', 'animateClose');                  }                  _accordionGroup.header.on('click', function () {                      if (!_accordionGroup.options.disabled) {                          _accordionGroup.options.open = !_accordionGroup.options.open;                          accordion.applyState(_accordionGroup);                      }                      $timeout(function () {                          scope.$apply();                      });                  });                  accordion.addGroup(_accordionGroup);              });          }      };  });
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name uiAccordion.directive:ngAccordionGroup
+ * @description
+ * # ngAccordionGroup
+ */
+angular.module('uiAccordion')
+  .directive('ngAccordionGroup', function (accordionGroup,$q,$timeout) {
+      return {
+          require: ['^ngAccordion', 'ngAccordionGroup'],
+          restrict: 'EA',
+          controller: 'NgAccordionGroupCtrl',
+          scope: {
+              options: '=?'
+          },
+          link: function (scope, element, attrs, controllers) {
+              var accordionCtrl, controller, accordion, _accordionGroup;
+              controller = controllers[1];
+              accordionCtrl = controllers[0];
+              accordion = accordionCtrl.accordion;
+              _accordionGroup = accordionGroup.createAccordionGrp();
+              if (!scope.options) {
+                  scope.options = accordionGroup.defaultAccordionGroupOptions();
+              }
+              _accordionGroup.options = angular.extend(accordionGroup.defaultAccordionGroupOptions(), scope.options);
+
+              scope.$watchCollection('options', function (n) {
+                  _accordionGroup.options = angular.extend(accordionGroup.defaultAccordionGroupOptions(), n);
+                  if (n && !n.disabled) {
+                      accordion.applyState(_accordionGroup);
+                  }
+              }, true);
+
+              $q.all([controller.getHeaderElement(), controller.getBodyElement()]).then(function (results) {
+                  _accordionGroup.header = results[0];
+                  _accordionGroup.body = results[1];
+                  if (scope.options.open) {
+                      _accordionGroup.$animate('show', 'beforeOpen', 'animateOpen');
+                  } else {
+                      _accordionGroup.$animate('hide', 'beforeHide', 'animateClose');
+                  }
+                  _accordionGroup.header.on('click', function () {
+                      if (!_accordionGroup.options.disabled) {
+                          _accordionGroup.options.open = !_accordionGroup.options.open;
+                          accordion.applyState(_accordionGroup);
+                      }
+                      $timeout(function () {
+                          scope.$apply();
+                      });
+                  });
+                  accordion.addGroup(_accordionGroup);
+              });
+
+          }
+      };
+  });
+
 'use strict';
 
 /**
@@ -118,12 +176,14 @@ angular.module('uiAccordion')
  * Service in the uiAccordion.
  */
 angular.module('uiAccordion')
-  .service('accordionGroup', function ($q,$log) {
+  .service('accordionGroup', function ($q,$log,$timeout) {
     function AccordionGroup() {
     }
     function noopPromise() {
       var def = $q.defer();
-      def.resolve();
+      $timeout(function(){
+        def.resolve('');
+      });
       return def.promise;
     }
 
